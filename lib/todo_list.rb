34e785d1803cef911f7694913a17f1f1803bff4b
Mike Pitre <[michaelepitre@gmail.com]>
2015-05-30 15:10:57 -0500

    added ability to edit todos

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -15,14 +15,15 @@ class TodoList
 
       puts
       puts "What would you like to do?"
-      puts "1) Exit 2) Add Todo 3) Mark Todo As Complete 4) Delete Todo"
+      puts "1) Exit 2) Add Todo 3) Mark Todo As Complete 4) Edit Todo 5) Delete Todo"
       print " > "
       action = gets.chomp.to_i
       case action
       when 1 then exit
       when 2 then add_todo
       when 3 then mark_todo
-      when 4 then delete_todo
+      when 4 then edit_todo
+      when 5 then delete_todo
       else
         puts "\a"
         puts "Not a valid choice"
@@ -44,6 +45,13 @@ class TodoList
   def mark_todo
     puts "Which todo have you finished?"
     Todo.update(get_input, completed: true)
+  end
+
+  def edit_todo
+    puts "Which todo do you want to update?"
+    todo_to_edit = get_input
+    puts "What should the todo be?"
+    Todo.update(todo_to_edit, entry: get_input)
   end
 
   def delete_todo
